9d44ff54a7d61a249fb2d56a94cadef7bb58f162
NAME <EMAIL>
2024-04-13 03:45:19 +0100

    shopify words are not allowed in postgres

--- shopify.sql
+++ shopify.sql
@@ -1,52 +1,61 @@
+create extension if not exists plpython3u;
+
 create schema if not exists shopify;
+create domain "text/html" as text;
+
 
 create table shopify.shop (
     shop text primary key,
-    access_token text not null
+    access_token text
 );
 
-alter database postgres set shopify.app_client_id to 'your_client_id_here';
-alter database postgres set shopify.app_client_secret to 'your_client_secret_here';
-alter database postgres set shopify.redirect_uri to 'https://yourapp.com/rpc/callback';
 
-create or replace function shopify.auth(shop text, state text)
-returns text
-language plpgsql
-as $$
-declare
-    client_id text := current_setting('shopify.app_client_id');
-    redirect_uri text := current_setting('shopify.redirect_uri');
-    scopes text := 'read_products,write_orders';
-    auth_url text;
-begin
-    auth_url := 'https://' || shop || '/admin/oauth/authorize?' ||
-                'client_id=' || client_id || '&' ||
-                'scope=' || scopes || '&' ||
-                'redirect_uri=' || redirect_uri || '&' ||
-                'state=' || state;
-    return auth_url;
-end
-$$;
-
-create or replace function shopify.callback(shop text, code text, hmac_received text, state text)
-returns void
-language plpython3u
-as $$
+create or replace function api.auth(_hmac text, _host text, _shop text, _timestamp text)
+returns "text/html" as $$
 import requests
+import hmac
+import hashlib
+import plpy
 
 client_id = plpy.execute("select current_setting('shopify.app_client_id')")[0]['current_setting']
 client_secret = plpy.execute("select current_setting('shopify.app_client_secret')")[0]['current_setting']
-response = requests.post(
-    'https://' || shop || '/admin/oauth/access_token',
-    data={
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'code': code
-    }
-)
-access_token = response.json().get('access_token')
-plpy.execute(
-    "insert into shopify.shop(shop, access_token) values (%s, %s) on conflict (shop) do update set access_token = excluded.access_token",
-    (shop, access_token)
-)
-$$;
+
+message = f"host={_host}&shop={_shop}&timestamp={_timestamp}"
+calculated_hmac = hmac.new(client_secret.encode(), msg=message.encode(), digestmod=hashlib.sha256).hexdigest()
+
+if _hmac != calculated_hmac:
+    raise Exception('HMAC validation failed')
+
+scopes = 'read_products,write_products'
+redirect_uri = 'https://usb-opposition-michael-sonic.trycloudflare.com/rpc/callback'
+state = 'nonce'
+
+auth_url = f"https://{_shop}/admin/oauth/authorize?client_id={client_id}&scope={scopes}&redirect_uri={redirect_uri}&state={state}"
+
+return f"<html><body><script>window.location.href='{auth_url}';</script></body></html>"
+$$ language plpython3u;
+
+create or replace function api.callback(_shop text, _code text)
+returns "text/html" as $$
+import requests
+import plpy
+
+client_id = plpy.execute("select current_setting('shopify.app_client_id')")[0]['current_setting']
+client_secret = plpy.execute("select current_setting('shopify.app_client_secret')")[0]['current_setting']
+
+response = requests.post(f"https://{_shop}/admin/oauth/access_token", json={
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "code": _code
+})
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+
+    # Store the access token in the database
+    plpy.execute(f"INSERT INTO shopify.shop (shop, access_token) VALUES ('{_shop}', '{access_token}') ON CONFLICT (shop) DO UPDATE SET access_token = '{access_token}'")
+
+    return f"<html><body><script>window.location.href='/rpc/success';</script></body></html>"
+else:
+    return f"<html><body><script>window.location.href='/rpc/error';</script></body></html>"
+$$ language plpython3u;
